95fe8269b021f9d95056144cd56e7427bde24c0d
dimaspaiva <dimasalpaiva@gmail.com>
2020-03-25 14:25:01 -0300

    create blog page

---
+++ src/pages/Blog.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+export default function Blog() {
+  return (
+    <div>
+      <Header />
+      <h1>Blog</h1>
+      <p>This is the blog area, where some posts will be added</p>
+      <Footer />
+    </div>
+  )
+}
