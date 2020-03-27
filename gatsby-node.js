35198938c475292703f58611ae3ee68de721fe93
dimaspaiva <dimasalpaiva@gmail.com>
2020-03-27 07:33:52 -0300

    create post page

---
+++ gatsby-node.js
@@ -0,0 +1,44 @@
+const path = require('path')
+
+module.exports.onCreateNode = ({ node, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = path.basename(node.fileAbsolutePath, '.md')
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: slug,
+    })
+  }
+}
+
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  const templatePath = path.resolve(__dirname, 'src', 'templates', 'blog.js')
+
+  const response = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  response.data.allMarkdownRemark.edges.forEach(it => {
+    createPage({
+      component: templatePath,
+      path: `blog/${it.node.fields.slug}`,
+      context: {
+        slug: it.node.fields.slug,
+      },
+    })
+  })
+}
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { graphql, useStaticQuery } from 'gatsby'
+import { graphql, useStaticQuery, Link } from 'gatsby'
 
 import Layout from '../components/Layout'
 
@@ -15,6 +15,9 @@ export default function Blog() {
             }
             html
             excerpt
+            fields {
+              slug
+            }
           }
         }
       }
@@ -28,6 +31,16 @@ export default function Blog() {
       <Layout>
         <h1 className="title">Blog</h1>
         <p>This is the blog area, where some posts will be added</p>
+        <ol>
+          {data.allMarkdownRemark.edges.map(it => (
+            <li key={Math.random()}>
+              <Link to={`/blog/${it.node.fields.slug}`}>
+                <h2>{it.node.frontmatter.title}</h2>
+                <p>{it.node.frontmatter.date}</p>
+              </Link>
+            </li>
+          ))}
+        </ol>
       </Layout>
     </div>
   )
